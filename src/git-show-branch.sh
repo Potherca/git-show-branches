b5544ee011a14360498d34e43c16c57ed7cf5109
Ben Peachey <ben.peachey@dealerdirect.nl>
2017-04-21 19:50:57 +0200

    Changes main files to source all logic.

--- src/git-show-branch.sh
+++ src/git-show-branch.sh
@@ -5,48 +5,11 @@ set -o errtrace # Exit on error inside any functions or sub-shells.
 set -o nounset  # Exit script on use of an undefined variable.
 set -o pipefail # Return exit status of the last command in the pipe that exited with a non-zero exit code
 
-if [[ -z ${g_sSourceDirectory-} ]];then
-    readonly g_sSourceDirectory="$(unset CDPATH && cd $( dirname $0 )/ && pwd -P )"
-fi
-
-source "${g_sSourceDirectory}/color.inc"
-source "${g_sSourceDirectory}/function.error.inc"
-source "${g_sSourceDirectory}/exit-codes.inc"
-source "${g_sSourceDirectory}/function.validate-directory.sh"
-
-declare g_iExitCode="${EX_OK}"
-
-git_show_branch() {
-
-    validate_directory "${1}"
-
-    local -r sDirectory=""$(unset CDPATH && cd $1/ && pwd -P )""
-    local -r sRootRepoHead="${2:-}"
-
-    pushd "${sDirectory}" > /dev/null
-
-    sRepo=$(basename "${sDirectory}")
-    sRepoHead="$(git rev-list --parents HEAD 2> /dev/null | tail -1 || echo '')"
-
-    if [[ "${sRootRepoHead}" = "${sRepoHead}" || "${sRepoHead}" = "" ]];then
-        sBranch="${COLOR_DIM}(not a git repo)${COLOR_RESET}"
-    else
-        sBranch=$(git symbolic-ref --quiet --short -q HEAD 2>/dev/null)
-
-        if [[ "${sBranch}" = 'master' ]];then
-            sBranch="${COLOR_GREEN}${sBranch}${COLOR_RESET}"
-        else
-            sBranch="${COLOR_YELLOW}${sBranch}${COLOR_RESET}"
-        fi
-    fi
-
-    popd > /dev/null
-
-    printf '%-24s: %s\n' "${sRepo}" "${sBranch}"
-
-    return ${EX_OK}
-}
-
+source "color.inc"
+source "exit-codes.inc"
+source "function.error.sh"
+source "function.validate-directory.sh"
+source 'function.git-show-branch.sh'
 
 if [[ ${BASH_SOURCE[0]} != $0 ]]; then
     export -f git_show_branch
--- src/git-show-branches.sh
+++ src/git-show-branches.sh
@@ -5,32 +5,12 @@ set -o errtrace # Exit on error inside any functions or sub-shells.
 set -o nounset  # Exit script on use of an undefined variable.
 set -o pipefail # Return exit status of the last command in the pipe that exited with a non-zero exit code
 
-readonly g_sSourceDirectory="$(unset CDPATH && cd $( dirname $0 )/ && pwd -P )"
-
-source "${g_sSourceDirectory}/git-show-branch.sh"
-
-git_show_branches() {
-
-    local sRootRepoHead sRepoHead sDirectory sBranch sRepo
-
-    if [[ "$#" -lt 1 ]];then
-        error 'One parameter expected' ${EX_NOT_ENOUGH_PARAMETERS}
-    else
-        validate_directory "${1}"
-
-        local -r sRootDirectory="$(unset CDPATH && cd $1 && pwd -P )"
-
-        pushd "${sRootDirectory}" > /dev/null
-        readonly sRootRepoHead="$(git rev-list --parents HEAD 2> /dev/null | tail -1 || echo '')"
-        popd > /dev/null
-
-        for sDirectory in ${sRootDirectory}/*/;do
-            git_show_branch "${sDirectory}" "${sRootRepoHead}"
-        done
-    fi
-
-    return ${EX_OK}
-}
+source "color.inc"
+source "exit-codes.inc"
+source "function.error.sh"
+source "function.validate-directory.sh"
+source 'function.git-show-branch.sh'
+source 'function.git-show-branches.sh'
 
 if [[ ${BASH_SOURCE[0]} != $0 ]]; then
     export -f git_show_branches
