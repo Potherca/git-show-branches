080b51758316673c48b2c7cd131b8f5510fd05c0
Ben Peachey <potherca@gmail.com>
2018-01-10 13:29:48 +0100

    Changes the `git-show-status` function so data gather and string compilation are separated.

--- src/function.git-show-status.sh
+++ src/function.git-show-status.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-# Inherit the parent's verbosity if it is present
+# --------------------------------------------------------------------------
+# Inherit the parent's settings if present
+# --------------------------------------------------------------------------
 # shellcheck disable=SC2154
 : export "${g_bVerbose:=false}"
 
 git-show-status() {
 
+    local -a aStatus=( )
     local sGitStatus sStatus sUnpushed
 
     readonly sGitStatus=$(git status 2> /dev/null)
@@ -16,48 +19,58 @@ git-show-status() {
     # --------------------------------------------------------------------------
     # Check for unpushed commits
     # --------------------------------------------------------------------------
-    if [[ "${sUnpushed}" != '' ]];then
-        sStatus="${sStatus}${COLOR_YELLOW}unpushed commits${COLOR_WHITE}/"
+    if [[ $(git log --branches --not --remotes) != '' ]];then
+        aStatus+=('unpushed commits')
     fi
 
     # --------------------------------------------------------------------------
     # Check if branch is up-to-date
     # --------------------------------------------------------------------------
-    if [[ ${g_bVerbose} = true ]];then
-        # @NOTE: As the wording has been changes (in v2.15.0) both formats need to be checked
-        if [[ -z "${sGitStatus##*Your branch is up-to-date with*}" || -z "${sGitStatus##*Your branch is up to date with*}" ]] ;then
-            sStatus="${sStatus}${COLOR_GREEN}up to date${COLOR_WHITE}/"
-        else
-            sStatus="${sStatus}${COLOR_YELLOW}incoming changes${COLOR_WHITE}/"
-        fi
+    # @NOTE: As the wording has been changes (in v2.15.0) both formats need to be checked
+    if [[ ! -z "${sGitStatus##*Your branch is up-to-date with*}" && ! -z "${sGitStatus##*Your branch is up to date with*}" ]] ;then
+        aStatus+=('incoming changes')
     fi
 
     # --------------------------------------------------------------------------
-    # Check for changes in the working directory
+    # Check for unstaged changes in the working directory
     # --------------------------------------------------------------------------
-    if [[ -z "${sGitStatus##*working directory clean*}" || -z "${sGitStatus##*working tree clean*}" ]];then
+    if [[ -z "${sGitStatus##*Changes not staged for commit:*}" ]];then
+        aStatus+=('unstaged changes')
+    fi
+
+    # --------------------------------------------------------------------------
+    # Check for staged changes in the working directory
+    # --------------------------------------------------------------------------
+    if [[ -z "${sGitStatus##*Changes to be committed:*}" ]];then
+        aStatus+=('staged changes')
+    fi
+
+    # --------------------------------------------------------------------------
+    # Check for untracked files in the working directory
+    # --------------------------------------------------------------------------
+    if [[ -z "${sGitStatus##*Untracked files:*}" ]];then
+        aStatus+=('untracked files')
+    fi
+
+    # --------------------------------------------------------------------------
+    # Build status string
+    # --------------------------------------------------------------------------
+    if [[ ${g_bVerbose} = true ]];then
+        if [[ "${aStatus[@]:-}" != *'incoming changes'* ]];then
+            sStatus="${sStatus}${COLOR_GREEN}up to date${COLOR_WHITE}/"
+        fi
+    fi
+
+    if [[ ${#aStatus[@]} = 0 ]];then
         sStatus="${sStatus}${COLOR_GREEN}clean${COLOR_WHITE}/"
     else
         if [[ ${g_bVerbose} = true ]];then
-            if [ -z "${sGitStatus##*Changes not staged for commit:*}" ] ;then
-                sStatus="${sStatus}${COLOR_YELLOW}unstaged changes${COLOR_WHITE}/"
-            fi
-
-            if [ -z "${sGitStatus##*Changes to be committed:*}" ] ;then
-                sStatus="${sStatus}${COLOR_YELLOW}staged changes${COLOR_WHITE}/"
-            fi
-
-            if [ -z "${sGitStatus##*Untracked files:*}" ] ;then
-                sStatus="${sStatus}${COLOR_YELLOW}untracked files${COLOR_WHITE}/"
-            fi
+            sStatus="${sStatus}$(printf "${COLOR_YELLOW}%s${COLOR_WHITE}/" "${aStatus[@]:-}")"
         else
             sStatus="${sStatus}${COLOR_YELLOW}not clean${COLOR_WHITE}/"
         fi
     fi
 
-    # --------------------------------------------------------------------------
-    # Format status string
-    # --------------------------------------------------------------------------
     if [[ "${sStatus}" != '' ]];then
         # Remove trailing slash `/`
         sStatus="${sStatus: : -1}"
