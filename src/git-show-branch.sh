42ca0ea01a649e1e46e106515038ccc50aed2e02
Ben Peachey <potherca@gmail.com>
2018-01-30 17:25:48 +0100

    Fixes bug caused by default parameter being set (that should not be).

--- src/git-show-branch.sh
+++ src/git-show-branch.sh
@@ -16,7 +16,7 @@ source 'function.git-show-status.sh'
 if [[ ${BASH_SOURCE[0]} != $0 ]]; then
     export -f git_show_branch
 else
-    git_show_branch "${@:-}"
+    git_show_branch "${@}"
     exit ${?}
 fi
 
